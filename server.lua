340dbebbf91c81d08ae2cca564d93e2bbd28e702
Brave Developments <89262662+Brave-developments@users.noreply.github.com>
2024-09-24 03:03:43 +1000

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,20 +1,24 @@
-QBCore = nil
 local QBCore = exports['qb-core']:GetCoreObject()
-TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
 
 RegisterServerEvent('toggleGodMode')
 AddEventHandler('toggleGodMode', function(godModeStatus)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
 
+    if not Player then
+        TriggerClientEvent('QBCore:Notify', src, "Player data not found.", 'error')
+        return
+    end
+
     if QBCore.Functions.HasPermission(src, "admin") then
         -- Fetch the player's name
         local playerName = Player.PlayerData.charinfo.firstname .. " " .. Player.PlayerData.charinfo.lastname
 
-        -- Trigger the client event to toggle god mode and send the player's name
-        TriggerClientEvent('setGodMode', src, godModeStatus, playerName)
+     
+        TriggerClientEvent('setGodMode', -1, src, godModeStatus, playerName)  
+        print(("[GodMode] %s (%s) toggled God Mode: %s"):format(playerName, src, tostring(godModeStatus)))
     else
-        -- Notify the player if they don't have the required permission
+       
         TriggerClientEvent('QBCore:Notify', src, "You do not have permission to use this command.", 'error')
     end
 end)
